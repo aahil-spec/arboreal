1aca1f8d5d366d03e6a73a8ff17d39dc233378da
BOSS <artworkbyfalak1@gmail.com>
2026-07-31 02:43:52 +0530

    added fern and fixed many bugs

--- scripts/sun.gd
+++ scripts/sun.gd
@@ -16,7 +16,7 @@ func _process(delta):
 	else:
 		intensity=0.0
 		
-	rotation_degrees.x=lerp(-90.0,-270.0,time/24.0)
+	rotation_degrees.x = (time / 24.0) * -360.0 + 90.0
 	
 	light_color=Color("1b2234").lerp(Color("ffffff"),intensity)
 	light_energy=lerp(0.2,1.0,intensity)
@@ -24,3 +24,5 @@ func _process(delta):
 	if world_env and world_env.environment:
 		world_env.environment.background_energy_multiplier = lerp(0.3, 1.0, intensity)
 		world_env.environment.ambient_light_energy = lerp(0.4, 1.0, intensity)
+	
+	
